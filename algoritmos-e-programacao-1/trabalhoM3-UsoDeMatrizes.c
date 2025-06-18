ef4e5242666a984d79d52f34a83b560b6fe561cd
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-06-18 20:20:24 -0300

    Proposta inicial, trabalho M3 - Parte 2

--- algoritmos-e-programacao-1/trabalhoM3-UsoDeMatrizes.c
+++ algoritmos-e-programacao-1/trabalhoM3-UsoDeMatrizes.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 #define n_linhas 3
-#define n_colunas 2
+#define n_colunas 3
 
 int main() {
-
-    // matrizes seguem a lógica de 3x2 em algorítmo = 2x3 em álgebra
-
-    // fileiras sempre subindo
-
-    // inferior esquerdo = 0,0 // superior direito igual 1,2 (em uma 3x2)
+    int escolhaExibicao;
 
 
-    int matriz[3][2] = {{1, 2}, {4, 5}, {7, 8}};
+
+    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // declaração inicial da matriz
 
 
-    for (int linha = 0; linha < n_linhas; linha++) {
+
+    for (int linha = 0; linha < n_linhas; linha++) { // for para imprimir a matriz COMPLETA
 
         for (int coluna = 0; coluna < n_colunas; coluna++){
-            printf("Endereco: [%d][%d] - ", linha, coluna);
+            printf("   %d", matriz[linha][coluna]);
         
 
         };
-        printf("\n");
+        printf("\n"); // para exibir a próxima linha
     };
 
+
+    // usuário poderá ver: 1- matriz completa, 2- diagonal principal, 3- triângulo superior, 4- triângulo inferior, 5- sair
+
+
+    printf("Menu:\n1- Matriz Completa\n2- Diagonal Principal\n3- Triangulo Superior\n4- Triangulo Inferior\n5- Sair\nEscolha uma opcao: ");
 
 
 
