d875a38bb30e711e9e47d40f24fbbb2f50e184ba
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-06-18 19:42:07 -0300

    Declaração inicial: trabalho semestral com uso de matrizes

---
+++ algoritmos-e-programacao-1/trabalhoM3-UsoDeMatrizes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    // matrizes seguem a lógica de 3x2 em algorítmo = 2x3 em álgebra
+
+    // fileiras sempre subindo
+
+    // inferior esquerdo = 0,0 // superior direito igual 1,2 (em uma 3x2)
+
+
+    int matriz[3][2] = {{1, 2}, {4, 5}, {7, 8}};
+
+
+
+
+
+
+    return 0;
+}
