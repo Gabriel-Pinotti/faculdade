fa85f25f9f47d58d706b9e730962ec98faa97fcd
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-08-07 21:40:53 -0300

    Adição funções e indentação

--- algoritmos-e-programacao-2/aula2.cpp
+++ algoritmos-e-programacao-2/aula2.cpp
@@ -15,9 +15,13 @@ int main() {
 
 int matriz[5][5];
 srand(time(0));
+int escolha_funcao = 0;
+
 
 #define colunas 5
 #define linhas 5
+
+// criar
 
 int linha_atual = 0;
 while (linha_atual < linhas) {
@@ -25,7 +29,7 @@ while (linha_atual < linhas) {
     int coluna_atual = 0;
     while (coluna_atual < colunas){
 
-        int aleatorio = rand()%10;
+        int aleatorio = rand()%21;
         matriz[linha_atual][coluna_atual] = aleatorio;
         ++coluna_atual;
     }
@@ -33,19 +37,69 @@ while (linha_atual < linhas) {
     ++linha_atual;
 };
 
+// imprimir
+
 linha_atual = 0;
 while (linha_atual < linhas) {
 
     int coluna_atual = 0;
     while (coluna_atual < colunas){        
 
-        cout << matriz[linha_atual][coluna_atual] << " ";
+        cout << matriz[linha_atual][coluna_atual] << "\t";
         ++coluna_atual;
     }
     cout << endl;
 
     ++linha_atual;
 };
+
+
+// selecionar função
+
+cout <<  endl << "   O que deseja fazer?" << endl << "1- Soma dos itens da linha 2" << endl << "2- Soma dos itens da coluna 4" << endl << "3- Soma dos itens da DP";
+cout << endl << "4- Soma dos itens da DS" << endl << "5- Soma de todos os itens da matriz" << endl << endl << "Resposta: ";
+cin >> escolha_funcao;
+
+cout << endl << endl;
+
+
+int tempSoma = 0;
+switch (escolha_funcao) {
+    case 1:
+        tempSoma = 0;
+        for(int i = 0; i < 5; ++i) {
+            tempSoma += matriz[1][i];
+        };
+        cout << tempSoma << endl;
+        break;
+
+    case 2:
+        tempSoma = 0;
+        for(int i = 0; i < 5; ++i) {
+            tempSoma += matriz[i][3];
+        };
+        cout << tempSoma << endl;        
+        break;
+
+    case 3:
+        tempSoma = 0;
+        linha_atual = 0;
+        while (linha_atual < linhas) {
+            int coluna_atual = 0;
+            while (coluna_atual < colunas){        
+                if (linha_atual == coluna_atual) {
+                    tempSoma += matriz[linha_atual][coluna_atual];
+                };
+                ++coluna_atual;
+            }
+            ++linha_atual;
+        };
+        cout << tempSoma << endl;
+        break;
+
+
+}
+
 
 return 0;
 }
