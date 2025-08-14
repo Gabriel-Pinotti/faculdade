60ecfeaef2261d4c8cd1b587eb0ea4715a25baee
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-08-14 20:05:12 -0300

    Criação de arquivos iniciais e organização de menu

---
+++ algoritmos-e-programacao-2/aula3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+// menu pra selecionar qual questao quer ver a resposta
+
+int main() {
+
+    // declaração de variáveis
+
+
+    cout << "======== MENU ========" << endl << endl << "1) Soma de dois numeros" << endl << "2) Dobrar valor" << endl;
+    cout << "3) Verificar se e par" << endl << "4) Maior valor entre dois numeros" << endl << "5) Contar ocorrencia de caractere" << endl;
+    cout << "6) Exibir menu" << endl << "7) Potencia (padrao 2)" << endl << "8) Converter para minusculo" << endl;
+    cout << "9) Substituir caractere" << endl << "10) Criar mascara de caracteres" << endl << "11) Media com validacao" << endl;
+    cout << "12) Normalizar texto" << endl << "13) Contar vogais e consoantes" << endl << "14) Maior frequencia de caractere" << endl;
+    cout << "15) Remover duplicatas" << endl << "16) Estatisticas de vetor" << endl << "17) Busca linear modular" << endl;
+    cout << "18) Verificar palindromo" << endl << "19) Mini calculadora" << endl << "20) Senha forte" << endl << endl;
+    cout << "Resposta:  " << endl << endl;
+
+
+
+
+
+    return 0;
+}
