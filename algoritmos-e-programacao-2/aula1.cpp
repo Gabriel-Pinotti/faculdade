d32a4640b07e6dc1952b2a07c6c7859390216cf9
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-08-14 20:38:48 -0300

    Teste em outros arquivos e criação das funções 1 e 2

--- algoritmos-e-programacao-2/aula1.cpp
+++ algoritmos-e-programacao-2/aula1.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+
+
 int main() {
     cout << "isso é um print" << endl;
     return 0;
--- algoritmos-e-programacao-2/aula3.cpp
+++ algoritmos-e-programacao-2/aula3.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int funcao1(int num1, int num2) {
+    return (num1 + num2);
+};
 
-// menu pra selecionar qual questao quer ver a resposta
+void funcao2(int &num){
+    num *=2;
+};
+
+
 
 int main() {
 
     // declaração de variáveis
-
+    int selecaoDeFuncao = 0;
 
     cout << "======== MENU ========" << endl << endl << "1) Soma de dois numeros" << endl << "2) Dobrar valor" << endl;
     cout << "3) Verificar se e par" << endl << "4) Maior valor entre dois numeros" << endl << "5) Contar ocorrencia de caractere" << endl;
@@ -16,11 +23,40 @@ int main() {
     cout << "12) Normalizar texto" << endl << "13) Contar vogais e consoantes" << endl << "14) Maior frequencia de caractere" << endl;
     cout << "15) Remover duplicatas" << endl << "16) Estatisticas de vetor" << endl << "17) Busca linear modular" << endl;
     cout << "18) Verificar palindromo" << endl << "19) Mini calculadora" << endl << "20) Senha forte" << endl << endl;
-    cout << "Resposta:  " << endl << endl;
+    cout << "Resposta:  ";
+    cin >> selecaoDeFuncao;
+
+
+    switch (selecaoDeFuncao){
+        case 1:
+            int func1_num1;
+            int func1_num2;
+            int func1_final;
+            cout << endl << endl << "Digite o primeiro número: ";
+            cin >> func1_num1;
+            cout << endl << "Digite o segundo número: ";
+            cin >> func1_num2;
+            func1_final = funcao1(func1_num1, func1_num2);
+            cout << endl << "Resultado: " << func1_final << endl << endl;
+            break;
+        case 2:
+            int func2_num;
+            cout << endl << endl << "Digite o número: ";
+            cin >> func2_num;
+            funcao2(func2_num);
+            cout << endl << "Resultado: " << func2_num << endl << endl;
+            break;
 
 
 
 
 
+
+
+
+
+    default:
+        return 0;
+    };
     return 0;
 }
