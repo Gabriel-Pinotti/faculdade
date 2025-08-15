c4055d0bbf04489010d12a02b9679ef41c104290
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-08-14 21:09:22 -0300

    Criação da função 3

--- algoritmos-e-programacao-2/aula3.cpp
+++ algoritmos-e-programacao-2/aula3.cpp
@@ -9,6 +9,14 @@ void funcao2(int &num){
     num *=2;
 };
 
+int funcao3(int num){ // 1 = par | 2 = ímpar
+    if (num % 2 == 1){
+        return 0    ;
+    } else {
+        return 1;
+    };
+};
+
 
 
 int main() {
@@ -16,8 +24,8 @@ int main() {
     // declaração de variáveis
     int selecaoDeFuncao = 0;
 
-    cout << "======== MENU ========" << endl << endl << "1) Soma de dois numeros" << endl << "2) Dobrar valor" << endl;
-    cout << "3) Verificar se e par" << endl << "4) Maior valor entre dois numeros" << endl << "5) Contar ocorrencia de caractere" << endl;
+    cout << "======== MENU ========" << endl << endl << "1) Soma de dois números" << endl << "2) Dobrar valor" << endl;
+    cout << "3) Verificar se é par" << endl << "4) Maior valor entre dois números" << endl << "5) Contar ocorrencia de caractere" << endl;
     cout << "6) Exibir menu" << endl << "7) Potencia (padrao 2)" << endl << "8) Converter para minusculo" << endl;
     cout << "9) Substituir caractere" << endl << "10) Criar mascara de caracteres" << endl << "11) Media com validacao" << endl;
     cout << "12) Normalizar texto" << endl << "13) Contar vogais e consoantes" << endl << "14) Maior frequencia de caractere" << endl;
@@ -46,10 +54,19 @@ int main() {
             funcao2(func2_num);
             cout << endl << "Resultado: " << func2_num << endl << endl;
             break;
-
-
-
-
+        case 3:
+            int func3_num;
+            int func3_resultado;
+            cout << endl << endl << "Digite o número: ";
+            cin >> func3_num;
+            func3_resultado = funcao3(func3_num);
+            if (func3_resultado == 1) {
+                cout << endl << "É par" << endl << endl;
+                break;
+            } else {
+                cout << endl << "É ímpar" << endl << endl;
+                break;
+            };
 
 
 
