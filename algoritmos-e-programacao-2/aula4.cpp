88c3b869abc6a281bedb969ce8b946be4ddde2d2
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-08-18 20:34:16 -0300

    Criação da função inicial de registro de vendas e estrutura de verificação no main

--- algoritmos-e-programacao-2/aula4.cpp
+++ algoritmos-e-programacao-2/aula4.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+using namespace std;
 
-
+double registrarVenda(double valor, bool reset=false){
+    static double valorVendas = 0;
+    if (reset){
+        valorVendas = 0;
+    };
+    valorVendas+= valor;
+    return valorVendas;
+};
 
 
 
 int main(){
 
+    double valorEmCaixa = registrarVenda(0);
+
+    cout << endl << "Valor em caixa: R$ " << valorEmCaixa << endl;
 
 
     return 0;
