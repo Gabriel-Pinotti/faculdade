b3993ea629ea335e1db64a61ea358f469e6a1218
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-01 19:52:27 -0300

    criação base e testes struct com template

--- algoritmos-e-programacao-2/aula5.cpp
+++ algoritmos-e-programacao-2/aula5.cpp
@@ -21,6 +21,21 @@ T verificar_maior(T a, T b){
 
 
 
+template <typename T>
+struct Calculadora{
+
+    T somar(T a, T b){
+        return a + b;
+    }
+
+    T multiplicar(T a, T b){
+        return a * b;
+    }
+
+};
+
+
+
 int main(){
 
     cout << "\n\ntestando\n\n" << "2 e 4 sempre\n";
@@ -29,9 +44,12 @@ int main(){
     cout << "\n\nSomar 3.4 e 4.19 = " << somar_t(3.4, 4.19);
     cout << "\n\nVerificar maior 3.3 e 5.6 = " << verificar_maior(3.3, 5.6);
     cout << "\n\nVerificar maior 9 e 4 = " << verificar_maior(9, 4);
+    
+    
+    Calculadora<double> calculadora_double;
+    cout << "\n\nUtilizando calcD, 2 + 4 = " << calculadora_double.somar(2, 4);
+    
+    
     cout << "\n\n\n";
-
-
-
     return 0;
 }
