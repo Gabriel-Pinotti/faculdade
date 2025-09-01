b953690ec8424b1ffebe7c3c2e5290af57b3c922
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-01 19:33:56 -0300

    calculadora simples base para exemplificação do professor

--- algoritmos-e-programacao-2/aula5.cpp
+++ algoritmos-e-programacao-2/aula5.cpp
@@ -3,9 +3,31 @@
 #include <iostream>
 using namespace std;
 
+double somar(double num1, double num2){
+return num1+num2;
+}
+
+double subtrair(double num1, double num2){
+return num1-num2;
+}
+
+double multiplicar(double num1, double num2){
+    return num1*num2;
+}
+
+double dividir(double num1, double num2){
+    return num1/num2;
+}
+
 int main(){
 
+    cout << "\n\ntestando\n\n" << "2 e 4 sempre\n";
 
+    cout << "\n\nSomar= " << somar(2, 4);
+    cout << "\n\nSubtrair= " << subtrair(2, 4);
+    cout << "\n\nMultiplicar= " << multiplicar(2, 4);
+    cout << "\n\nDividir= " << dividir(2, 4);
+    cout << "\n\n\n";
 
 
 
