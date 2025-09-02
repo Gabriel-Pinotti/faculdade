0429a289f77ab7ef53b3f5a1747ed6c4e3b74a5c
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-01 21:00:10 -0300

    Ajuste declaração de array e nomenclatura

--- algoritmos-e-programacao-2/aula6.cpp
+++ algoritmos-e-programacao-2/aula6.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void imprimirArray(int vetor[], int tamanho){
-    cout << "\n\nVetor = ";
+void imprimirArray(int array[], int tamanho){
+    cout << "\n\nArrray = ";
     for (int i = 0; i < tamanho; ++i){
-        cout << vetor[i] << " ";   
+        cout << array[i] << " ";   
     }
     cout << "\n\n";
 };
 
-void bubblesort(int vetor[], int tamanho){
+void bubblesort(int array[], int tamanho){
     int temp;
     bool trocou = false;
     do {
         for (int i = 0; i < tamanho-1; ++i){
-            if (vetor[i] > vetor [i+1]){
-                temp = vetor[i];
-                vetor[i] = vetor[i+1];
-                vetor[i+1] = temp;
+            if (array[i] > array [i+1]){
+                temp = array[i];
+                array[i] = array[i+1];
+                array[i+1] = temp;
                 trocou = true;
                 goto repetir;
             } else {
                 trocou = false;
             }
         }
-        repetir: ;
+        repetir:;
     } while (trocou);
 };
 
 int main(){
-    int vetorbase[5] = {2, 5, 8, 3, 1};
-    int tamanhoarray = (sizeof(vetorbase) / sizeof(vetorbase[0]));
+    int arraybase[] = {2, 5, 8, 3, 1, 6, 4, 0, 7, 9};
+    int tamanhoarray = (sizeof(arraybase) / sizeof(arraybase[0]));
 
-    imprimirArray(vetorbase, tamanhoarray);
+    imprimirArray(arraybase, tamanhoarray);
 
     cout << "[ORGANIZANDO]";
-    bubblesort(vetorbase, tamanhoarray);
+    bubblesort(arraybase, tamanhoarray);
 
-    imprimirArray(vetorbase, tamanhoarray);
+    imprimirArray(arraybase, tamanhoarray);
 
     cout << "\n\n";
     return 0;
