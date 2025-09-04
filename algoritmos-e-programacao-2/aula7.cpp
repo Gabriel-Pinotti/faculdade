b4f3691d70f8213ed9539a58ef72545578371be8
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-04 20:08:48 -0300

    sobrecarga da função triangulo e deleção da struct

--- algoritmos-e-programacao-2/aula7.cpp
+++ algoritmos-e-programacao-2/aula7.cpp
@@ -2,27 +2,28 @@
 using namespace std;
 
 
-struct Calculadora {
-    double triangulo(double base, double altura){
-        return (base*altura)/2;
-    };
-
-    double retangulo(double base, double altura){
-        return base*altura;
-    };
-    double circulo(double raio){
-        return raio*raio*3.14;
-    }
+double triangulo(double base, double altura){ // considerando altura
+    return (base*altura)/2;
 };
+double triangulo(double lado){ // considerando apenas lado (equilatero)
+    return (lado*lado)*(1.73/4);
+}
+double retangulo(double base, double altura){
+    return base*altura;
+};
+double circulo(double raio){
+    return raio*raio*3.14;
+}
+
 
 
 
 int main(){
 
-    Calculadora calc;
-    cout << "\nTriangulo = " << calc.triangulo(5.7, 2);
-    cout << "\nRetangulo = " << calc.retangulo(8, 9);
-    cout << "\nCirculo = " << calc.circulo(2);
+    cout << "\nTriangulo com altura = " << triangulo(5.7, 2);
+    cout << "\nTriangulo sem altura = " << triangulo(2);
+    cout << "\nRetangulo = " << retangulo(8, 9);
+    cout << "\nCirculo = " << circulo(2);
 
 
     cout << "\n\n";
