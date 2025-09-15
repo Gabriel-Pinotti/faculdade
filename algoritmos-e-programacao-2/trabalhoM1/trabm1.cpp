7aeae31fb83f60c0ce134e4664053db37c40cbb6
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-15 19:56:47 -0300

    colisão dos fantasmas com as pílulas e criação do menu de dificuldade

--- algoritmos-e-programacao-2/trabalhoM1/trabm1.cpp
+++ algoritmos-e-programacao-2/trabalhoM1/trabm1.cpp
@@ -7,8 +7,8 @@ using namespace std;
     
 // variaveis globais
 int qtdBombas = 3;
-
-
+int pontos = 0;
+int dificuldade = 0;
 
 vector<string> mapa = { // mapa com vetor pq sim <3 facilitando a vida
      "####################",
@@ -21,26 +21,55 @@ vector<string> mapa = { // mapa com vetor pq sim <3 facilitando a vida
      "###      #######   #",
      "####################",
   } ;   
-  
+
+int selecionarDificuldade(){
+    int tempSelecionarDif;
+    system("clear");
+    cout << "Selecione a dificuldade (padrao: facil)\n";
+    cout << "1- Facil\n2- Medio\n3- Dificil\n\nResposta: ";
+    cin >> tempSelecionarDif;
+    if (tempSelecionarDif < 1 || tempSelecionarDif > 3){tempSelecionarDif = 1;}
+    return tempSelecionarDif;
+}
+
+
+
 bool espacoLivre (int l, int c) { 
     return mapa [l][c] != '#'; // impede que o jogadro saia das paredes
 }
      
 struct posicao {int l, c;};
-posicao jogador = {2,1}; // guardar dados do jogador  
+posicao jogador = {2,1};
 
 vector <posicao> pilulas;
 void pospilulas() {
     pilulas.clear();
-    pilulas.push_back({1, 4});
-};
+    vector<posicao> todos = {
+        {1,2}, {6,15}, {1,10}, {2,18}, {7,16}, {7, 17}, {5,5}, {4, 12}, {7, 4}, {7, 7}
+    };
+    int quantidade = 0;
+    if (dificuldade == 1) quantidade = 10;
+    else if (dificuldade == 2) quantidade = 8;
+    else if (dificuldade == 3) quantidade = 5;
+    for (int i = 0; i < quantidade; i++) {
+        pilulas.push_back(todos[i]);
+    }
+}
 
 vector <posicao> fantasma;
-void posfantasma () { // TODO criar um desse para cada nível de dificuldade
-    fantasma.clear(); // limpando o vetor
-    fantasma.push_back ({2,7});//push_back adiciona algo no vetor e as coordenadas dizem onde
-    fantasma.push_back ({6,10});
-    
+void posfantasma() {
+    fantasma.clear();
+    vector<posicao> todos = {
+        {2,7}, {6,10}, {1,12}, {2,17}, {7,18}, {6,5}
+    };
+    int quantidade = 0;
+    if (dificuldade == 1) quantidade = 3;
+    else if (dificuldade == 2) quantidade = 5;
+    else if (dificuldade == 3) quantidade = 6;
+    // adiciona as posiçoes correspondentes
+    for (int i = 0; i < quantidade; i++) {
+        fantasma.push_back(todos[i]);
+    }
 }
 
 struct structbomba {int l, c; int contador;};
@@ -54,6 +83,13 @@ void posicBomba(){
 bool temBomba (int l, int c) {
     for(auto& b : bombas){
         if (b.l == l && b.c == c){return true;};
+    }
+    return false;
+};
+
+bool temPilula (int l, int c) {
+    for(auto& p : pilulas){
+        if (p.l == l && p.c == c){return true;};
     }
     return false;
 };
@@ -76,7 +112,7 @@ void movjog (char cmd) {
 }
 
 void movfantasma () {
-    for (auto &f : fantasma) {// to com preguica de por srand
+    for (auto &f : fantasma) {
         int dir = rand() %4; // da um valor aleatorio no intervalo da %
         posicao prox = f;
         if (dir == 0) prox.l--; // cima
@@ -84,7 +120,7 @@ void movfantasma () {
         if (dir == 2) prox.c--; // esquerda
         if (dir == 3) prox.c++; // direita
 
-        if (espacoLivre(prox.l, prox.c) && temBomba(prox.l, prox.c) == false) {
+        if (espacoLivre(prox.l, prox.c) && temBomba(prox.l, prox.c) == false && temPilula(prox.l, prox.c) == false) {
             f = prox; // mover fantasma
         }       
     }
@@ -92,19 +128,20 @@ void movfantasma () {
 
 void desenhar() {
     system("clear"); 
+    cout << "\nPontuacao: " << pontos;
     cout << "\nBombas disponiveis: " << qtdBombas << "\n\n";
     vector<string> img = mapa;
 
     for (auto &f : fantasma) { //desenhando fantasma
         img[f.l][f.c] = 'G';
     }
-    for (auto &p : pilulas){
+    for (auto &p : pilulas){ // pilulas
         img[p.l][p.c] = '%';
     }
-    for (auto &b : bombas){
+    for (auto &b : bombas){ // bombas
         img[b.l][b.c] = 'b';
     }
-    img[jogador.l][jogador.c] = 'P'; // desenhando jogador
+    img[jogador.l][jogador.c] = 'P'; // jogador
     
 
     for (auto &line : img) cout << line << "\n";
@@ -112,17 +149,18 @@ void desenhar() {
 
 void telaDeVitoria(){
     system("clear");
-    cout << "Voce venceu!!\n\n";
+    cout << "Voce venceu!!\nPontuacao: " << pontos << "\n\n"; // TODO bonus de tempo (jogadas restantes)
     return;
 }
 
 
 int main (){ 
-  posfantasma();
-  pospilulas();
-  srand (time(0));
+    dificuldade = selecionarDificuldade();
+    posfantasma();
+    pospilulas();
+    srand (time(0));
       
-      while (true) {  
+    while (true) {  
         
         desenhar();
         cout << "\n---Use WASD para mover o jogador, B para posicionar uma bomba e Q para sair\n**Legenda**\n-#: Parede";
@@ -147,6 +185,7 @@ int main (){
                     int dc = abs(fantasma[j].c - bombas[i].c);
                     if (dl <= 1 && dc <= 1) { // diferença de ate 1 em linha e coluna
                         fantasma.erase(fantasma.begin() + j); // mata o fantasma
+                        pontos += 200;
                         j--;
                     }
                 }
@@ -163,8 +202,8 @@ int main (){
         for (auto& f : fantasma) { //colisao dos fantasmas
             if (f.l == jogador.l && f.c == jogador.c) {
                 system("clear");
-                cout << "Voce foi pego por um fantasma! Game Over.\n\n";
-                // TODO cout << "Pontos: " << pontos << "\n";
+                cout << "Voce foi pego por um fantasma! Game Over.\n";
+                cout << "Pontos: " << pontos << "\n\n";
                 return 0; 
             }
         }
