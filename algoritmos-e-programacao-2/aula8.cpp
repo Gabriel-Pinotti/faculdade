91cbf0f947cdac34ab8eae98477180a51fe89084
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-18 17:41:52 -0300

    Junção de struct com template e exemplificação separada de template

--- algoritmos-e-programacao-2/aula8.cpp
+++ algoritmos-e-programacao-2/aula8.cpp
@@ -1,22 +1,38 @@
 #include <iostream>
 using namespace std;
 
+
+template <typename TYPE>
 struct nomestruct {
-    int i;
-    float j;
+    TYPE i;
+    TYPE j;
     void calcular(){
         cout << "\ni * j = " << i*j;
     }
 };
 
+
+template <typename T>
+T somar(T a, T b){
+    return a + b;
+}
+
+
+
 int main(){
-    nomestruct varDeStruct;
+    nomestruct<int> varDeStruct;
 
     varDeStruct.i = 2;
-    varDeStruct.j = 2.6;
+    varDeStruct.j = 7;
     cout << "\ni = " << varDeStruct.i;
     cout << "\nj = " << varDeStruct.j;
     varDeStruct.calcular();
+
+    cout << "\n\n";
+
+    cout << somar<int>(2, 3) << endl;
+    cout << somar<float>(2.3, 3.2) << endl;
+
 
 
 
