227e155b7a22f9e09444ef4aa90a316cee147a5c
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-22 19:41:59 -0300

    criação de ponteiros bases, testes de saída e anotações de especificação de ponteiros

--- algoritmos-e-programacao-2/aula9.cpp
+++ algoritmos-e-programacao-2/aula9.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    // ponteiros: variáveis que o valor é o endereço de outra variável
+    // essenciais para passagem de parâmetros por referência, gerenciamento de memória, estruturas dinâmicas etc.
 
-
+    // exemplo:
+    int a = 10;
+    int *p = &a;
+    a = 15;
+    cout << *p << endl;
 
     cout << "\n\n";
     return 0;
