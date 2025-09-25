92b7bfe0263ec7c27a6ade139a2f94011af4d887
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-25 19:53:26 -0300

    Geração de função de falha de segmentação para exemplificação

--- algoritmos-e-programacao-2/aula11.cpp
+++ algoritmos-e-programacao-2/aula11.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+#include <cstdlib> // necesário para: malloc, 
 using namespace std;
+
+
+// void funcao_recursiva(int i = 0){ // essa função gera falha de segmentação
+//     char arraygrande[100000];
+
+//     cout << "Nova execução da função\n";
+//     arraygrande[0] = 'a';
+
+//     funcao_recursiva();
+// }
+
 
 int main(){
 
+    // endereçamento não necessariamente é sequencial, salvo por vetores, que busca um espaço
+    //      na memória que possa armazenar todos os elementos sequencialmente.
+
+    // um stack overflow ocorre quando um programa tenta utilizar mais espaços da memória
+    //      do que há disponíveis.
+
+    cout << "\nIniciando o programa\n";
+    
+    //funcao_recursiva();
 
 
-
-    cout << "\n\n";
+    cout << "\nExecutou até o final\n";
     return 0;
 }
