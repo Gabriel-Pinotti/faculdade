bd70d41751530d9b6162327d84b9e3c3049f0004
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-09-29 17:14:42 -0300

    organização de arquivos para melhor visibilidade e bugs parcialmente corrigidos, como a comparação do número de entradas e capacidade total, corrigida em aula11-atv4.cpp

---
+++ algoritmos-e-programacao-2/parte2/aula11-atv4.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+// TODO corrigir bugs de lógica de armazenamento
+
+int main(){
+
+    // crie um programa que simule a entrada de dados de um usuário sem saber a quantidade de antemão. o programa deve:
+    //  1. começar com um array dinâmico com capacidade inicial de 2 inteiros
+    //  2. em um laço, pergunte ao usuário por um número. digitar 0 deve encerrar a entrada
+    //  3. a cada número inserido, se a capacidade do array for atingida, use realloc para dobrar o tamanho do arrray
+    //  4. armazene o número no array
+    //  5. ao final da entrada, imprima todos os números armazenados no array e, por fim, libere a memória
+
+    // etapa 1
+    int *listaint = (int*)malloc(2 * sizeof(int));
+
+
+    int capacidade = 2;
+    int numentradas = 0;
+    int tempentrada;
+    while(true){
+        // etapa 2
+        cout << "\nDigite um número: ";
+        cin >> tempentrada;
+        if (tempentrada == 0){
+            break;
+        }
+        // etapa 4
+        listaint[numentradas] = tempentrada;
+        numentradas++;
+        // etapa 3
+        if (numentradas == capacidade){
+            capacidade*=2;
+            // cout << "realloc para " << capacidade << " | numentradas = " << numentradas << endl;; (teste para validação do sistema de verificação)
+            listaint = (int*)realloc(listaint, capacidade* sizeof(int));
+        }
+    }
+
+
+
+    // etapa 5.1
+    // for (int i = 0; i < sizeof(listaint)/sizeof(listaint[0]); i++){
+    //     cout << "\ni = " << i << endl;
+    //     cout << listaint[i] << " ";
+    // }
+
+
+    // etapa 5.2
+    free(listaint);
+    cout << "\n\n";
+    return 0;
+}
