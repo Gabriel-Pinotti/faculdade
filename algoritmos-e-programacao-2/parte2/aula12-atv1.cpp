1dd0ec3a4d3e66218faf3c09243106110d3e8532
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-10-06 20:26:29 -0300

    Sequência de Fibonacci agora com função recursiva e loop estabelecido na main()

--- algoritmos-e-programacao-2/parte2/aula12-atv1.cpp
+++ algoritmos-e-programacao-2/parte2/aula12-atv1.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
 
-// int fibonacci(int n){
-//     // futuramente será feita uma função aqui
-// }
+
+int fibonacci_recursivo(int n){
+    if (n<2)
+        return 1;
+    else
+        return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2);
+}
 
 
 
 
 int main(){
-    int fibonacci[50] = {1, 1};
+    // int fibonacci[50] = {1, 1};
 
-    for (int i = 0; i < 15; i++){
-        fibonacci[i+2] = fibonacci[i]+fibonacci[i+1];
+    // for (int i = 0; i < 15; i++){
+    //     fibonacci[i+2] = fibonacci[i]+fibonacci[i+1];
+    // }
+
+    // for (int i = 0; i < 17; i++){
+    //     cout << fibonacci[i] << " | ";
+    // }
+    int n = 10;
+
+    cout << "Sendo n = " << n << "\n -Sequência de Fibonacci até n\n\n";
+
+    for (int i = 0; i < n; i++){
+        cout << fibonacci_recursivo(i) << "  ";
     }
-
-    for (int i = 0; i < 17; i++){
-        cout << fibonacci[i] << " | ";
-    }
-
 
 
     cout << "\n\n";
