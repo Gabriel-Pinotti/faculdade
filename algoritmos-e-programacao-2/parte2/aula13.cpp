e74d140221ea648e23016e823f07bda4758fe947
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-10-09 19:51:15 -0300

    criação da estrutura base do quick sort com recursividade em função void

--- algoritmos-e-programacao-2/parte2/aula13.cpp
+++ algoritmos-e-programacao-2/parte2/aula13.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void quicksort(int *vetor, int inicio, int fim){
+    int pivo = vetor[inicio];
+    int esquerda = inicio;
+    int direita = fim;
+
+    while (esquerda <= direita){
+        while (vetor[esquerda] < pivo)
+            esquerda++;
+        
+        while (vetor[direita] > pivo)
+            direita--;
+
+        if (esquerda <= direita){
+            int temp = vetor[esquerda];
+            vetor[esquerda] = vetor[direita];
+            vetor[direita] = temp;
+
+            esquerda++;
+            direita--;
+        }
+    }
+    if (inicio < direita){
+        quicksort(vetor, inicio, direita);
+    }
+    if (esquerda < fim){
+        quicksort(vetor, esquerda, fim);
+    }
+}
 
 
 int main(){
-    
+
 
 
     cout << "\n\n";
