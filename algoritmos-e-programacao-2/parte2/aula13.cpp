a45e2b3abf527bd090e1034bb54a2dc5eb841a48
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-10-09 19:56:44 -0300

    função para impressão e ajustes no main para criação do vetor aleatoriamente

--- algoritmos-e-programacao-2/parte2/aula13.cpp
+++ algoritmos-e-programacao-2/parte2/aula13.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctime>
 using namespace std;
 
 void quicksort(int *vetor, int inicio, int fim){
@@ -31,8 +32,29 @@ void quicksort(int *vetor, int inicio, int fim){
     }
 }
 
+void imprimir(int *vetor, int tam){
+    for(int i = 0; i < tam; i++){
+        cout << vetor[i] << " ";
+    }
+    cout << endl << endl;
+}
+
 
 int main(){
+    srand(time(NULL));
+
+    int tam = 8;
+    int *vetor = (int*)malloc(tam*sizeof(int));
+
+    for(int i =0; i < tam; i++){
+        vetor[i] = rand() %100;
+    }
+
+    cout << "\nVetor antes do quicksort: " << endl;
+    imprimir(vetor, tam);
+    quicksort(vetor, 0, tam-1);
+    cout << "\nVetor depois do quicksort: " << endl;
+    imprimir(vetor, tam);
 
 
 
