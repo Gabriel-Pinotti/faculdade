2450d29c9fb25835fdd4fc815560da3ebf6e7d08
Gabriel-Pinotti <gabriel.apinotti06@gmail.com>
2025-11-06 21:32:39 -0300

    organização de movimentação e colisão em funções para manter o código limpo, assim como a criação de variáveis em #define para evitar a repetição de passagem de parâmetros desnecessária; Criação da função pressionamento_teclas() para movimentação do jogador com "WASD" e setas do teclado

--- algoritmos-e-programacao-2/parte4/snake/main.cpp
+++ algoritmos-e-programacao-2/parte4/snake/main.cpp
@@ -2,71 +2,89 @@
 #include <iostream>
 using namespace std;
 
-#define UP 0
+#define UP 0 // Direcionamento com ints
 #define DOWN 1
 #define LEFT 2
 #define RIGHT 3
 
+#define SCREEN_WIDTH 800 // Define o tamanho da janela
+#define SCREEN_HEIGHT 600
+
+#define SPEED 250 // Define a velocidade do círculo
+#define RADIUS 15 // Define o tamanho do círculo
+
+void colisao(Vector2 &pos){
+        if (pos.x < RADIUS){ // colidiu na parede da esquerda
+            pos.x = RADIUS;
+        }
+        if (pos.x > SCREEN_WIDTH-RADIUS){ // colidiu na parede da direita
+            pos.x = SCREEN_WIDTH-RADIUS;
+        }
+        if (pos.y < RADIUS){ // colidiu na parede de cima
+            pos.y = RADIUS;
+        }
+        if (pos.y > SCREEN_HEIGHT-RADIUS){ // colidiu na parede de baixo
+            pos.y = SCREEN_HEIGHT-RADIUS;
+        }
+}
+
+void movimentacao(int lastPos, Vector2 &pos, float &dt){
+        switch (lastPos){ 
+            case UP:
+                pos.y-=SPEED * dt;
+                break;
+
+            case DOWN:
+                pos.y+=SPEED * dt;
+                break;
+
+            case LEFT:
+                pos.x-=SPEED * dt;
+                break;
+
+            case RIGHT:
+                pos.x+=SPEED * dt;
+                break;
+        }
+}
+
+void pressionamento_teclas(int &lastPos){
+    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)){
+        lastPos = UP;
+    }
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)){
+        lastPos = DOWN;
+    }
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)){
+        lastPos = RIGHT;
+    }
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
+        lastPos = LEFT;
+    }
+}
+
 
 
 int main() {
-    const int screenWidth = 800; // Define dimensões da janela
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Movimentação básica");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Movimentação básica");
     SetTargetFPS(60); // Define FPS do programa
-
-
-    Vector2 pos = {screenWidth/2.0f, screenHeight/2.0f};
-    const float speed = 250.0f;
-    const float radius = 15.0f;
+    Vector2 pos = {SCREEN_WIDTH/2.0f, SCREEN_HEIGHT/2.0f}; // Posição inicial
     int lastPos = RIGHT;
 
 
     while (!WindowShouldClose()) { // Roda isso enquanto estiver aberto
-
         float dt = GetFrameTime();
-        switch (lastPos){ // Verifica lastPos (direcionamento) para mover o círculo
-            case UP:
-                pos.y-=speed * dt;
-                break;
+        
+        movimentacao(lastPos, pos, dt); // Verifica lastPos (direcionamento) para mover o círculo
+        colisao(pos); // Não deixa o círculo sair da tela, comparando seu tamanho à margem
+        DrawCircleV(pos, RADIUS, BLUE); // DrawCircleV(onde, tamanho, cor);
 
-            case DOWN:
-                pos.y+=speed * dt;
-                break;
-
-            case LEFT:
-                pos.x-=speed * dt;
-                break;
-
-            case RIGHT:
-                pos.x+=speed * dt;
-                break;
-        }
-
-        if (pos.x < radius){ // Não deixa o círculo sair da tela, comparando seu tamanho à margem
-            pos.x = radius;
-        }
-        if (pos.x > screenWidth-radius){
-            pos.x = screenWidth-radius;
-        }
-        if (pos.y < radius){
-            pos.y = radius;
-        }
-        if (pos.y > screenHeight-radius){
-            pos.y = screenHeight-radius;
-        }
-
-        DrawCircleV(pos, radius, BLUE); // DrawCircleV(onde, tamanho, cor);
-
-
-
-
-
+        pressionamento_teclas(lastPos);
 
 
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawFPS(screenWidth - 90, 10);
+        DrawFPS(SCREEN_WIDTH - 90, 10); // Mostra o FPS na tela;
         EndDrawing();
     }
 
