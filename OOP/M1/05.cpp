95728ce434cafb17d6d9ca3c5da1ef503fe89cd6
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-05-05 19:38:27 -0300

    add: info to 05.cpp

--- OOP/M1/05.cpp
+++ OOP/M1/05.cpp
@@ -3,8 +3,34 @@ using namespace std;
 // atributos e métodos estáticos em classe
 // vive na classe, é compartilhado entre todas as instâncias
 
+class Retangulo {
+    private:
+        float altura = 1;
+        float largura = 1;
+    public:
+        static float area;
+        Retangulo();
+        Retangulo(float altura, float largura) {
+            this->altura = altura;
+            this->largura = largura;
+        }
+        
+        float calculaarea() {
+            return altura * largura;
+        }
+
+        void imprime() {
+            cout << "Altura: " << altura << "\nLargura: " << largura << "\n";
+            cout << "\nÁrea: " << area;
+        }
+};
+float Retangulo::area = 45;
 
 int main(){
+
+    Retangulo* r1 = new Retangulo(2, 3);
+    Retangulo::area = r1->calculaarea();
+    r1->imprime();
 
 
     cout << "\n\n";
