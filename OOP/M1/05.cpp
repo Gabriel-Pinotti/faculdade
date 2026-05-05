e89e8d518015aa054ff1e86b6102651cdd5165a0
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-05-05 19:22:23 -0300

    add: 05.cpp

---
+++ OOP/M1/05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// atributos e métodos estáticos em classe
+// vive na classe, é compartilhado entre todas as instâncias
+
+
+int main(){
+
+
+    cout << "\n\n";
+    return 0;
+}
