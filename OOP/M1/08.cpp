f61416f27543e727131e7384bc9e0c5bef29c656
gabriel-pinotti <gabriel.apinotti06@gmail.com>
2026-05-12 19:41:29 -0300

    add: 08.cpp

---
+++ OOP/M1/08.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+    private:
+        string nome;
+
+    public:
+        // construtor
+        Animal(string nome) {this->nome=nome;};
+
+        virtual void comer() {
+            cout << "\nAnimal comeu";
+        };
+};
+
+class Mamifero : public Animal {
+    public:
+        // construtor
+        Mamifero(string nome) : Animal(nome) {}
+
+        void comer() {
+            cout << "\nMamífero comeu";
+        };
+};
+
+class Cachorro : public Mamifero {
+    public:
+        // construtor
+        Cachorro(string nome) : Mamifero(nome) {}
+
+        void comer() {
+            cout << "\nCachorro comeu";
+        };
+
+};
+
+
+int main()
+{
+    Animal *m = new Mamifero("Morcego"); 
+    // imprime Animal comeu pois é um ponteiro pra animal (no caso de não ser virtual)
+    // se for virtual em animal, imprime mamífero
+    // se não for virtual em animal, imprime animal
+    // se não existir em mamífero, mesmo sendo virtual em animal, imprime animal (pois busca na pai caso não exista na filho)
+    m->comer();
+
+    Animal *c = new Cachorro("Boris");
+    c->comer();
+
+
+
+    
+    cout << "\n\n";
+    return 0;
+}
